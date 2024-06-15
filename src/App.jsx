e722d33bfc4aab889f4adcc26091eedefef49652
Mohab Mohammed <92528415+mohabmohamed44@users.noreply.github.com>
2024-06-15 18:10:16 +0300

    Final Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// src/App.jsx
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/store';
+
+function App() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="container">
+      <h1>Counter: {count}</h1>
+      <button className='increment-btn' onClick={() => dispatch(increment())}>Increment</button>
+      <button className='decrement-btn' onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
